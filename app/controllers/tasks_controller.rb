819ad312d3c619e1268379afd35eb9a2500f81e2
ilyishka <strilets.piupiu@gmail.com>
2023-10-30 15:52:03 +0200

    Added tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,24 @@
 class TasksController < ApplicationController
+    def index
+        @tasks = Task.order(created_at: :desc)
+    end
+
+    def create
+        @task = Task.new(task_params)
+        if @task.save
+            redirect_to root_path, notice: "Задача была создана"
+        else 
+            render :new
+        end
+    end
+
+    def new
+        @task = Task.new
+    end
+
+    private
+
+    def task_params
+        params.required(:task).permit(:body, :end, :begin)
+    end
 end
