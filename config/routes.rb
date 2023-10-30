33bad1a693d8909878063a8c92db8552b20572a6
ilyishka <strilets.piupiu@gmail.com>
2023-10-30 14:13:37 +0200

    add controller Tasks

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :users
+  root 'pages#index'
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
